b9ce78ba9bdaedfdb475d6f2c2364f1f63e7a13e
gordoste <gordoste@iinet.net.au>
2025-01-08 00:33:54 +1100

    Remove Arduino core libraries from platformio.ini

--- source/ESP32-PSU-pio/src/main.cpp
+++ source/ESP32-PSU-pio/src/main.cpp
@@ -1,1 +1,11 @@
+// Putting Arduino core libraries in here allows PIO's dependency-finder to get them
+#include <EEPROM.h>
+#include <ESPmDNS.h>
+#include <FS.h>
+#include <SPI.h>
+#include <Update.h>
+#include <Wire.h>
+#include <WiFi.h>
+#include <WebServer.h>
+
 #include "../../Control_C_ADS_Touch_3_5/Control_C_ADS_Touch_3_5.ino"
